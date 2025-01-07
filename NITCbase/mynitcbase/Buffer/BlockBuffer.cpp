852c4003b1cb514400b97751329f5c845c175cf1
1-Nishanth-1 <nishanthrn1@gmail.com>
2025-01-07 19:05:57 +0530

    2nd stage without questions

--- NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
+++ NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
@@ -1,4 +1,52 @@
 #include "BlockBuffer.h"
-
 #include <cstdlib>
 #include <cstring>
+
+BlockBuffer::BlockBuffer(int blockNum)
+{
+    this->blockNum = blockNum;
+}
+
+RecBuffer::RecBuffer(int blockNum) : BlockBuffer::BlockBuffer(blockNum) {}
+
+int BlockBuffer::getHeader(struct HeadInfo *head)
+{
+    unsigned char buffer[BLOCK_SIZE];
+
+    Disk::readBlock(buffer, this->blockNum);
+
+    memcpy(&head->pblock, buffer + 4, 4);
+    memcpy(&head->lblock, buffer + 8, 4);
+    memcpy(&head->rblock, buffer + 12, 4);
+    memcpy(&head->numEntries, buffer + 16, 4);
+    memcpy(&head->numAttrs, buffer + 20, 4);
+    memcpy(&head->numSlots, buffer + 24, 4);
+
+    return SUCCESS;
+}
+
+int RecBuffer::getRecord(union Attribute *rec, int slotNum)
+{
+    struct HeadInfo head;
+    this->getHeader(&head);
+    int attrCount = head.numAttrs;
+    int slotCount = head.numSlots;
+
+    unsigned char buffer[BLOCK_SIZE];
+
+    Disk::readBlock(buffer, this->blockNum);
+
+    /* record at slotNum will be at offset HEADER_SIZE + slotMapSize + (recordSize * slotNum)
+     - each record will have size attrCount * ATTR_SIZE
+     - slotMap will be of size slotCount
+    */
+    int recordSize = attrCount * ATTR_SIZE;
+
+    int offset = HEADER_SIZE + slotCount + (slotNum * recordSize);
+
+    unsigned char *slotPointer = buffer + offset;
+
+    memcpy(rec, slotPointer, recordSize);
+
+    return SUCCESS;
+}
--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -5,20 +5,60 @@
 #include <cstring>
 #include <iostream>
 using namespace std;
-int main(int argc, char *argv[]) {
-  Disk disk_run;
 
+void stage1()
+{
   unsigned char buffer[BLOCK_SIZE];
-  Disk::readBlock(buffer, 7000);
   char message[] = "hello";
-  memcpy(buffer + 20, message, 6);
+  memcpy(buffer, message, 6);
   Disk::writeBlock(buffer, 7000);
 
   unsigned char buffer2[BLOCK_SIZE];
   char message2[6];
   Disk::readBlock(buffer2, 7000);
-  memcpy(message2, buffer2 + 20, 6);
-  cout << message2<<endl;
+  memcpy(message2, buffer2, 6);
+  cout << message2 << endl;
+}
+
+int main(int argc, char *argv[])
+{
+  Disk disk_run;
+
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
+
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  for (int i = 0; i < relCatHeader.numEntries; i++)
+  {
+
+    Attribute relCatRecord[RELCAT_NO_ATTRS];
+
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    int Counter = 0;
+
+    for (int j = 0; j < attrCatHeader.numEntries; j++)
+    {
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+
+      attrCatBuffer.getRecord(attrCatRecord, j);
+
+      if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0)
+      {
+
+        const char *attrtype = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+        printf("\t%s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrtype);
+            }
+    }
+    printf("\n");
+  }
 
   return 0;
 }
